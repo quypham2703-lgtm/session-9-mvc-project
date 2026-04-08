dace9f4d20697bd9c52672b03aae535672a20806
quypham2703-lgtm <quypham2703@gmail.com>
2026-04-08 23:56:46 +0700

    Update ProductController.php

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -1,1 +1,12 @@
+<?php
+require_once "../app/models/ProductModel.php";
 
+class ProductController {
+
+    public function index() {
+        $model = new ProductModel();
+        $products = $model->all();
+
+        require_once "../app/views/product/index.php";
+    }
+}
